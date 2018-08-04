cbeca0fcc68f74fb295bf6a01d99084b71aa1056
Henri Ngo <henri.ngo@live.fr>
2018-08-04 13:24:26 +0200

    Added Zoom and Acceleration

--- ThirdPersonController/Controller.gd
+++ ThirdPersonController/Controller.gd
@@ -1,12 +1,12 @@
-extends Node
+extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 export(NodePath) var PlayerPath  = ""
 export(float) var MovementSpeed = 10
-export var MouseSensitivity = 2
-
+export(float) var Acceleration = 3
+export(float) var MouseSensitivity = 2
+export(float) var RotationLimit = 45
+export(float) var MaxZoom = 0.5
+export(float) var MinZoom = 1.5
 
 var Player
 var InnerGimbal
@@ -14,6 +14,8 @@ var Direction = Vector3()
 var Rotation = Vector2()
 var gravity = Vector3(0,-10,0)
 var Movement = Vector3()
+var ZoomFactor = 1
+var Speed = Vector3()
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -21,41 +23,54 @@ func _ready():
 	InnerGimbal =  $InnerGimbal
 	pass
 
-func _input(event):
-	
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
-	
-	if Input.is_action_pressed("move_forward"):
-		Direction.z =-1
-	elif Input.is_action_pressed("move_backward"):
-		Direction.z =1
-	elif Input.is_action_just_released("move_forward") or Input.is_action_just_released("move_backward"):
-		Direction.z = 0
-		
-	if Input.is_action_pressed("stafe_left"):
-		Direction.x =-1
-	elif Input.is_action_pressed("stafe_right"):
-		Direction.x =1
-	elif Input.is_action_just_released("stafe_left") or Input.is_action_just_released("stafe_right"):
-		Direction.x=0
+func _unhandled_input(event):
 	
 	if event is InputEventMouseMotion :
 		Rotation = event.relative
-	else:
-	 	Rotation = Vector2()
+	
+	if event is InputEventMouseButton:
+		match event.button_index:
+			BUTTON_WHEEL_UP:
+				ZoomFactor -= 0.05
+			BUTTON_WHEEL_DOWN:
+				ZoomFactor += 0.05
+		ZoomFactor = clamp(ZoomFactor, MaxZoom, MinZoom)
+
+	if event is InputEventKey and event.pressed:
+		match event.scancode:
+			KEY_ESCAPE:
+					get_tree().quit()
+			KEY_Z:
+				Direction.z -= 1
+			KEY_S:
+				Direction.z += 1
+			KEY_Q:
+				Direction.x -= 1
+			KEY_D:
+				Direction.x += 1
+	if event is InputEventKey and not event.pressed:
+		match event.scancode:
+			KEY_Z:
+				Direction.z += 1
+			KEY_S:
+				Direction.z -= 1
+			KEY_Q:
+				Direction.x += 1
+			KEY_D:
+				Direction.x -= 1
+	Direction.z = clamp(Direction.z, -1,1)
+	Direction.x = clamp(Direction.x, -1,1)
 
 func _physics_process(delta):
 	Player.rotate_y(deg2rad(-Rotation.x)*delta*MouseSensitivity)
 	InnerGimbal.rotate_x(deg2rad(-Rotation.y)*delta*MouseSensitivity)
+	InnerGimbal.rotation_degrees.x = clamp(InnerGimbal.rotation_degrees.x, -RotationLimit, RotationLimit)
+	Rotation = Vector2()
 	
-	Movement = Player.transform.basis * (MovementSpeed * Direction.normalized())
-	Movement += gravity 
+	var MaxSpeed = MovementSpeed *Direction.normalized()
+	Speed = Speed.linear_interpolate(MaxSpeed, delta * Acceleration)
+	Movement = Player.transform.basis * (Speed)
+	Movement += gravity
 	
+	InnerGimbal.set_scale(Vector3(ZoomFactor,ZoomFactor,ZoomFactor))
 	Player.move_and_slide(Movement,Vector3(0,1,0))
-	
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
